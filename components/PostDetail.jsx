f53cfc4474fbe85723be5f8d3996f6c7580eb1ac
Garvit Agrawal <agarvit74@gmail.com>
2022-12-31 12:24:34 +0530

    Updated PostDetail Sections

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,11 +1,117 @@
-import React from 'react'
+import React from "react";
+import moment from "moment/moment";
 
-const PostDetail = () => {
+const PostDetail = ({ post }) => {
+  const getContentFragment = (index, text, obj, type) => {
+    let modifiedText = text;
+
+    if (obj) {
+      if (obj.bold) {
+        modifiedText = <b key={index}>{text}</b>;
+      }
+
+      if (obj.italic) {
+        modifiedText = <em key={index}>{text}</em>;
+      }
+
+      if (obj.underline) {
+        modifiedText = <u key={index}>{text}</u>;
+      }
+    }
+
+    switch (type) {
+      case "heading-three":
+        return (
+          <h3 key={index} className="text-xl font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        );
+      case "paragraph":
+        return (
+          <p key={index} className="mb-8">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        );
+      case "heading-four":
+        return (
+          <h4 key={index} className="text-md font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h4>
+        );
+      case "image":
+        return (
+          <img
+            key={index}
+            alt={obj.title}
+            height={obj.height}
+            width={obj.width}
+            src={obj.src}
+          />
+        );
+      default:
+        return modifiedText;
+    }
+  };
+
   return (
-    <div className='bg bg-transparent shadow-2xl rounded-lg lg:p-8 pb-12 mb-8'>
-        <h1>Post details</h1>
-    </div>
-  )
-}
+    <div className="bg bg-transparent shadow-2xl rounded-lg lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md mb-6">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="object-top h-full w-full rounded-t-lg"
+        />
+      </div>
+      <div className="px-4 lg:px-0">
+        <div className="flex items-center mb-8 w-full">
+          <div className="flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+            <img
+              alt={post.author.name}
+              height="30px"
+              width="30px"
+              className="align-middle rounded-full"
+              src={post.author.photo.url}
+            />
+            <p className="text-white inline align-middle ml-2 text-lg">
+              {post.author.name}
+            </p>
+          </div>
+          <div className="font-medium text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="h-6 w-6 inline mr-2"
+              viewBox="0 0 16 16"
+            >
+              {" "}
+              <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />{" "}
+            </svg>
+            <span className="align-middle">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </span>
+          </div>
+        </div>
+        <h1 className="mb-8 text-3xl font-semibold text-center transition duration-700 transform hover:text-yellow-500 hover:scale-125">
+          {post.title}
+        </h1>
+        {post.content.raw.children.map((typeObj, index) => {
+          const children = typeObj.children.map((item, itemIndex) =>
+            getContentFragment(itemIndex, item.text)
+          );
 
-export default PostDetail
+          return getContentFragment(index, children, typeObj, typeObj.type);
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default PostDetail;
