15c8d6f90bfa9db106e540603f6dc3061ffdc408
garvit74 <63715496+garvit74@users.noreply.github.com>
2023-01-08 12:30:07 +0530

    Update Container Size in Recent Post Section

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -31,9 +31,9 @@ const PostWidget = ({ categories, slug }) => {
                     <Image
                         unoptimized
                         alt={post.title}
-                        height="60"
-                        width="60"
-                        className=" cursor-pointer align-middle rounded-lg transition duration-1000 transform hover:-translate-y-3"
+                        height="45"
+                        width="45"
+                        className=" cursor-pointer align-middle rounded-2xl transition duration-1000 transform hover:-translate-y-3"
                         src={post.featuredImage.url}
                     /> 
                 </div>
